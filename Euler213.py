886420e33028c9640a6de5ba93f1f0136b0e05c3
Vasilis Kasimatis <v.kasimatis@jrtechnologies.com>
2017-07-31 17:09:47 +0300

    Fixes for Euler213.py

--- Euler213.py
+++ Euler213.py
@@ -22,24 +22,112 @@
 # Xwros katastasewn..
 # Anti na ypologizeis olo ton xwro katastasewn, mporeis na ypologizeis apla 0,1, opou to "1" shmainei >0 fleas
 A=list()
-DIM = 30
+DIM = 3
 A = [[[0.0 if x!=1 else 1.0 for x in xrange(2)] for i in xrange(DIM)] for j in xrange(DIM)]
 # A = [[[0 if x!=1 else 1 for x in xrange(DIM*DIM+1)] for i in xrange(DIM)] for j in xrange(DIM)]
 
 
+# # #
+# # #
+# # #
+
+
+
+
 # # # # #
 # # # # #
 # # # # #
 # # # # #
 # # # # #
+
+
+# P(A
+import itertools as it
+
+# # #
+# # #
+# # #
+# We must preserve the state... during iterations
 def bell():
     global A
     global DIM
     for i in xrange(DIM):
         for j in xrange(DIM):
             if i>1 and i<(DIM-2) and j>1 and j<(DIM-2):
-                #First calculate zero:
+                # P(A and B) = P(A) * P(B/A)
+                # P(A1UA2UA3...B)=P(A1|B)+P(A2|B)+P(A3|B)
+                A[i][j][1] = A[i+1][j][1] * 1/4 + A[i-1][j][1] * 1/4 + A[i][j+1][1] * 1/4 + A[i][j-1][1] * 1/4
+            # ####
+            elif 1 < i and i < (DIM-2) and j == (DIM-2):
+                A[i][j][1] = A[i + 1][j][1] * 1 / 4 + A[i - 1][j][1] * 1 / 4 + A[i][j + 1][1] * 1 / 3 + A[i][j - 1][1] * 1 / 4
+
+            elif 1 < i and i < (DIM-2) and j == (DIM-1):
+                A[i][j][1] = A[i + 1][j][1] * 1 / 3 + A[i - 1][j][1] * 1 / 3 + A[i][j - 1][1] * 1 / 4
+
+            elif 1 < i and i < (DIM-2) and j == 1:
+                A[i][j][1] = A[i + 1][j][1] * 1 / 4 + A[i - 1][j][1] * 1 / 4 + A[i][j - 1][1] * 1 / 3 + A[i][j + 1][1] * 1 / 4
+
+            elif 1 < i and i < (DIM-2) and j == 0:
+                A[i][j][1] = A[i + 1][j][1] * 1 / 3 + A[i - 1][j][1] * 1 / 3 + A[i][j + 1][1] * 1 / 4
+
+            #     #########
+            elif 1 < j and j < (DIM-2) and i == (DIM-2):
+                A[i][j][1] = A[i][j+1][1] * 1 / 4 + A[i][j-1][1] * 1 / 4 + A[i+1][j][1] * 1 / 3 + A[i-1][j][1] * 1 / 4
+
+            elif 1 < j and j < (DIM-2) and i == (DIM-1):
+                A[i][j][1] = A[i][j+1][1] * 1 / 3 + A[i][j-1][1] * 1 / 3 + A[i-1][j][1] * 1 / 4
+
+            elif 1 < j and j < (DIM-2) and i == 1:
+                A[i][j][1] = A[i][j+1][1] * 1 / 4 + A[i][j-1][1] * 1 / 4 + A[i-1][j][1] * 1 / 3 + A[i+1][j][1] * 1 / 4
+
+            elif 1 < j and j < (DIM-2) and i == 0:
+                A[i][j][1] = A[i][j+1][1] * 1 / 3 + A[i][j-1][1] * 1 / 3 + A[i+1][j][1] * 1 / 4
+
+            # ######
+            elif i == (DIM-2) and j == (DIM-2):
+                A[i][j][1] = A[i][j + 1][1] * 1 / 3 + A[i][j - 1][1] * 1 / 4 + A[i + 1][j][1] * 1 / 3 + A[i - 1][j][1] * 1 / 4
+
+            elif i == (DIM-2) and j == (DIM-1):
+                A[i][j][1] = A[i][j - 1][1] * 1 / 4 + A[i + 1][j][1] * 1 / 2 + A[i - 1][j][1] * 1 / 3
+
+            elif i == (DIM-1) and j == (DIM-1):
+                A[i][j][1] = A[i][j - 1][1] * 1 / 3 + A[i - 1][j][1] * 1 / 3
+                # # #
+                # # #
+                # # *
+
+            elif i == (DIM-1) and j == (DIM-2):
+                A[i][j][1] = A[i][j - 1][1] * 1 / 3 + A[i][j + 1][1] * 1 / 2 + A[i - 1][j][1] * 1 / 4
+            # ########
+            elif i == 1 and j == 1:
+                A[i][j][1] = A[i][j - 1][1] * 1 / 3 + A[i][j + 1][1] * 1 / 4 + A[i - 1][j][1] * 1 / 3 + A[i + 1][j][1] * 1 / 4
+
+            elif i == 1 and j == 0:
+                A[i][j][1] = A[i][j + 1][1] * 1 / 4 + A[i - 1][j][1] * 1 / 2 + A[i + 1][j][1] * 1 / 3
+
+            elif i == 0 and j == 0:
+                A[i][j][1] = A[i][j + 1][1] * 1 / 3 + A[i + 1][j][1] * 1 / 3
+
+            elif i == 0 and j == 1:
+                A[i][j][1] = A[i][j + 1][1] * 1 / 3 + A[i][j - 1][1] * 1 / 2 + A[i + 1][j][1] * 1 / 4
+
+            A[i][j][0] = 1 - A[i][j][1]
+
+def bell2():
+    global A
+    global DIM
+    for i in xrange(DIM):
+        for j in xrange(DIM):
+            if i>1 and i<(DIM-2) and 1 < j < (DIM - 2):
+                # all combinations..
                 A[i][j][0] = A[i+1][j][1] * 3/4 + A[i-1][j][1] * 3/4 + A[i][j+1][1] * 3/4 + A[i][j-1][1] * 3/4
+                # per = [
+                #         A[i+1][j][1] * 3/4, A[i-1][j][1] * 3/4, A[i][j+1][1] * 3/4, A[i][j-1][1] * 3/4,
+                #         A[i + 1][j][0], A[i - 1][j][0], A[i][j + 1][0], A[i][j - 1][0]
+                #        ]
+                # s = 0.0
+                # for k in xrange(4):
+                #     s += it.combinations()
 
             # ####
             elif 1 < i < (DIM-2) and j == (DIM-2):
@@ -61,13 +149,13 @@ def bell():
                              + A[i-1][j][1] * 3 / 4
 
             elif 1 < j < (DIM-2) and i == (DIM-1):
-                A[i][j][0] = A[i ][j+1][1] * 2 / 3 + A[i ][j-1][1] * 2 / 3 + A[i-1][j][1] * 3 / 4
+                A[i][j][0] = A[i][j+1][1] * 2 / 3 + A[i ][j-1][1] * 2 / 3 + A[i-1][j][1] * 3 / 4
 
             elif (DIM-2) > j > 1 == i:
-                A[i][j][0] = A[i ][j+1][1] * 3 / 4 + A[i][j-1][1] * 3 / 4 + A[i-1][j][1] * 2 / 3 + A[i+1][j][1] * 3 / 4
+                A[i][j][0] = A[i][j+1][1] * 3 / 4 + A[i][j-1][1] * 3 / 4 + A[i-1][j][1] * 2 / 3 + A[i+1][j][1] * 3 / 4
 
             elif (DIM-2) > j > 0 == i:
-                A[i][j][0] = A[i ][j+1][1] * 2 / 3 + A[i][j-1][1] * 2 / 3 + A[i+1][j][1] * 3 / 4
+                A[i][j][0] = A[i][j+1][1] * 2 / 3 + A[i][j-1][1] * 2 / 3 + A[i+1][j][1] * 3 / 4
 
             # ######
             elif i == (DIM-2) and j == (DIM-2):
@@ -94,16 +182,36 @@ def bell():
             elif i == 0 and j == 1:
                 A[i][j][0] = A[i][j + 1][1] * 2 / 3 + A[i][j - 1][1] * 1 / 2 + A[i + 1][j][1] * 3 / 4
 
-for j in xrange(50):
+            A[i][j][1] = 1 - A[i][j][0]
+
+
+
+for j in xrange(1):
     bell()
 
 
+
+
 exp = 0
-for i in xrange(30):
-    for j in xrange(30):
+
+for i in xrange(DIM):
+    for j in xrange(DIM):
         exp += A[i][j][0]
 
-print exp
+print("%.6f" % exp)
+
+print A[0][0][0] # P(1) =  1/3 + 1/3 = 2/3 => P(0)= 1-2/3 = 1/3 = 0.33333
+print A[2][2][0] # P(1) =  1/3 + 1/3 = 2/3 => P(0)= 1-2/3 = 1/3 = 0.33333
+
+# # #
+# # #
+# # #
+
+
+# print A[1][0][0]
+# print A[0][1][1]
+
+
 
                 # # # # #
                 # # # # #
