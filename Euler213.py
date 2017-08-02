1213d8b6b3eedd3ff75c94b8c413ee0f0c7d0c6c
Vasilis Kasimatis <v.kasimatis@jrtechnologies.com>
2017-08-02 16:09:06 +0300

    Work in progress for Euler213.py

--- Euler213.py
+++ Euler213.py
@@ -21,12 +21,31 @@
 # simulation? Las Vegas trial.. but only for 50 bells.
 # Xwros katastasewn..
 # Anti na ypologizeis olo ton xwro katastasewn, mporeis na ypologizeis apla 0,1, opou to "1" shmainei >0 fleas
+
+from operator import mul
+
+
 A=list()
 DIM = 5
-A = [[[0.0 if x!=1 else 1.0 for x in xrange(2)] for i in xrange(DIM)] for j in xrange(DIM)]
+A = [[[1.0 if x==0 or x==2 else 1.0 for x in xrange(3)] for i in xrange(DIM)] for j in xrange(DIM)]
 # A = [[[0 if x!=1 else 1 for x in xrange(DIM*DIM+1)] for i in xrange(DIM)] for j in xrange(DIM)]
 
+for i in xrange(DIM):
+    dirI =2
+    if i == DIM - 1 or i == 0:
+        dirI -= 1
+    for j in xrange(DIM):
+        dirJ = 2
+        if j == DIM - 1 or j == 0:
+            dirJ -= 1
+        _dir = dirI + dirJ
+        A[i][j][2] = (_dir-1.0)/_dir
+
+
+
 import copy
+import itertools as it
+
 
 
 # # #
@@ -58,6 +77,17 @@ import itertools as it
 # # #
 # # #
 # We must preserve the state... during iterations
+
+def comb(ll):
+    R1 = [x[1]*x[2] for x in ll]
+    R2 = [x[0] for x in ll]
+    _sum = 0.0
+    for k in xrange(len(R1) + 1):
+        for a1 in it.combinations(R1, k):
+            for a2 in it.combinations(R2, len(R2) - k):
+                _sum += reduce(mul, a2, 1) * reduce(mul, a1, 1)
+    return _sum
+
 def bell():
     global A
     global DIM
@@ -66,37 +96,51 @@ def bell():
     for i in xrange(DIM):
         for j in xrange(DIM):
             if i>1 and i<(DIM-2) and j>1 and j<(DIM-2):
-                _A[i][j][1] = 1-(A[i+1][j][1] * 3/4 * A[i-1][j][1] * 3/4 * A[i][j+1][1] * 3/4 * A[i][j-1][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i+1][j][1] * 3/4 * A[i-1][j][1] * 3/4 * A[i][j+1][1] * 3/4 * A[i][j-1][1] * 3/4)
+                _A[i][j][0] = comb([A[i+1][j], A[i-1][j], A[i][j+1], A[i][j-1]])
 
+                # _A[i][j][0] = A[i+1][j][1] * 3/4 * A[i-1][j][1] * 3/4 * A[i][j+1][1] * 3/4 * A[i][j-1][1] * 3/4
+
+
+            #  H pi8anothta na exeis '0' einai h pi8anothta na mhn sou dwsei out o A out o B
             # ###############################
             if 1 < i and i < (DIM-2) and j == (DIM-2):
-                _A[i][j][1] = 1-(A[i + 1][j][1] * 3/4 * A[i - 1][j][1] * 3/4 * A[i][j + 1][1] * 2/3 * A[i][j - 1][1] * 3/4)
+                _A[i][j][0] = comb((A[i + 1][j], A[i - 1][j], A[i][j + 1], A[i][j - 1]))
+                # _A[i][j][1] = 1-(A[i + 1][j][1] * 3/4 * A[i - 1][j][1] * 3/4 * A[i][j + 1][1] * 2/3 * A[i][j - 1][1] * 3/4)
 
             if 1 < i and i < (DIM-2) and j == (DIM-1):
-                _A[i][j][1] = 1-(A[i + 1][j][1] * 2/3 * A[i - 1][j][1] * 2/3 * A[i][j - 1][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i + 1][j][1] * 2/3 * A[i - 1][j][1] * 2/3 * A[i][j - 1][1] * 3/4)
+                _A[i][j][0] = comb((A[i + 1][j], A[i - 1][j],A[i][j - 1]))
 
             if 1 < i and i < (DIM-2) and j == 1:
-                _A[i][j][1] = 1-(A[i + 1][j][1] * 3/4 * A[i - 1][j][1] * 3/4 * A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i + 1][j][1] * 3/4 * A[i - 1][j][1] * 3/4 * A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 3/4)
+                _A[i][j][0] = comb((A[i + 1][j], A[i - 1][j], A[i][j - 1],A[i][j + 1]))
 
             if 1 < i and i < (DIM-2) and j == 0:
-                _A[i][j][1] = 1-(A[i + 1][j][1] * 2/3 * A[i - 1][j][1] * 2/3 * A[i][j + 1][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i + 1][j][1] * 2/3 * A[i - 1][j][1] * 2/3 * A[i][j + 1][1] * 3/4)
+                _A[i][j][0] = comb((A[i + 1][j], A[i - 1][j], A[i][j + 1]))
 
             #     #########
             if 1 < j and j < (DIM-2) and i == (DIM-2):
-                _A[i][j][1] = 1-(A[i][j+1][1] * 3/4 * A[i][j-1][1] * 3/4 * A[i+1][j][1] * 2/3 * A[i-1][j][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i][j+1][1] * 3/4 * A[i][j-1][1] * 3/4 * A[i+1][j][1] * 2/3 * A[i-1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j + 1], A[i][j - 1], A[i + 1][j],  A[i - 1][j]))
 
             if 1 < j and j < (DIM-2) and i == (DIM-1):
-                _A[i][j][1] = 1-(A[i][j+1][1] * 2/3 * A[i][j-1][1] * 2/3 * A[i-1][j][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i][j+1][1] * 2/3 * A[i][j-1][1] * 2/3 * A[i-1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j+1], A[i][j-1], A[i-1][j]))
 
             if 1 < j and j < (DIM-2) and i == 1:
-                _A[i][j][1] = 1-(A[i][j+1][1] * 3/4 * A[i][j-1][1] * 3/4 * A[i-1][j][1] * 2/3 * A[i+1][j][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i][j+1][1] * 3/4 * A[i][j-1][1] * 3/4 * A[i-1][j][1] * 2/3 * A[i+1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j+1], A[i][j-1], A[i-1][j], A[i+1][j]))
 
             if 1 < j and j < (DIM-2) and i == 0:
-                _A[i][j][1] = 1-(A[i][j+1][1] * 2/3 * A[i][j-1][1] * 2/3 * A[i+1][j][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i][j+1][1] * 2/3 * A[i][j-1][1] * 2/3 * A[i+1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j+1], A[i][j-1], A[i+1][j]))
 
             # ######## i== DIM-2 ##################################################################################
             if i == (DIM-2) and j == (DIM-2):
-                _A[i][j][1] = 1-(A[i][j + 1][1] * 2/3 * A[i][j - 1][1] * 3/4 * A[i + 1][j][1] * 2/3 * A[i - 1][j][1] * 3/4)
+                # _A[i][j][1] = 1-(A[i][j + 1][1] * 2/3 * A[i][j - 1][1] * 3/4 * A[i + 1][j][1] * 2/3 * A[i - 1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j + 1], A[i][j - 1], A[i + 1][j], A[i - 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
@@ -104,7 +148,8 @@ def bell():
                 # # # # #
 
             if i == (DIM-2) and j == (DIM-1):
-                _A[i][j][1] = 1-(A[i][j - 1][1] * 3/4 * A[i + 1][j][1] * 1/2 * A[i - 1][j][1] * 2/3)
+                # _A[i][j][1] = 1-(A[i][j - 1][1] * 3/4 * A[i + 1][j][1] * 1/2 * A[i - 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j - 1], A[i + 1][j],A[i - 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
@@ -112,14 +157,16 @@ def bell():
                 # # # # #
 
             if i == (DIM-2) and j == 0:
-                _A[i][j][1] = 1- (A[i][j + 1][1] * 3/4 * A[i + 1][j][1] * 1/2 * A[i - 1][j][1] * 2/3)
+                # _A[i][j][1] = 1- (A[i][j + 1][1] * 3/4 * A[i + 1][j][1] * 1/2 * A[i - 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j + 1],A[i + 1][j], A[i - 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
 #               * # # # #
                 # # # # #
             if i == (DIM-2) and j == 1:
-                _A[i][j][1] = 1- (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 3/4 * A[i - 1][j][1] * 3/4 * A[i + 1][j][1] * 2/3)
+                # _A[i][j][1] = 1- (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 3/4 * A[i - 1][j][1] * 3/4 * A[i + 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j - 1], A[i][j + 1], A[i - 1][j], A[i + 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
@@ -129,7 +176,8 @@ def bell():
             # ######## i== DIM-1 ##################################################################################
 
             if i == (DIM-1) and j == (DIM-1):
-                _A[i][j][1] = 1-(A[i][j - 1][1] * 2/3 * A[i - 1][j][1] * 2/3)
+                # _A[i][j][1] = 1-(A[i][j - 1][1] * 2/3 * A[i - 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j - 1],A[i - 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
@@ -137,7 +185,8 @@ def bell():
                 # # # # *
 
             if i == (DIM-1) and j == (DIM-2):
-                _A[i][j][1] = 1- (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 1/2 * A[i - 1][j][1] * 3/4)
+                # _A[i][j][1] = 1- (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 1/2 * A[i - 1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j - 1], A[i][j + 1], A[i - 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
@@ -145,7 +194,8 @@ def bell():
                 # # # * #
 
             if i == (DIM - 1) and j == 0:
-                _A[i][j][1] = 1 - (A[i][j + 1][1] * 2/3 * A[i - 1][j][1] * 2/3)
+                # _A[i][j][1] = 1 - (A[i][j + 1][1] * 2/3 * A[i - 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j + 1], A[i - 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
@@ -153,7 +203,8 @@ def bell():
 #               * # # # #
 
             if i == (DIM - 1) and j == 1:
-                _A[i][j][1] = 1 - (A[i][j - 1][1] * 1/2 * A[i][j + 1][1] * 2/3 * A[i - 1][j][1] * 3/4)
+                # _A[i][j][1] = 1 - (A[i][j - 1][1] * 1/2 * A[i][j + 1][1] * 2/3 * A[i - 1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j - 1], A[i][j + 1], A[i - 1][j]))
                 # # # # #
                 # # # # #
                 # # # # #
@@ -164,13 +215,16 @@ def bell():
             # ######## i == 1 ##################################################################################
 
             if i == 1 and j == 1:
-                _A[i][j][1] = 1 - (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 3/4 * A[i - 1][j][1] * 2/3 * A[i + 1][j][1] * 3/4)
+                # _A[i][j][1] = 1 - (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 3/4 * A[i - 1][j][1] * 2/3 * A[i + 1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j - 1], A[i][j + 1], A[i - 1][j], A[i + 1][j]))
 
             if i == 1 and j == 0:
-                _A[i][j][1] = 1 - (A[i][j + 1][1] * 3/4 * A[i - 1][j][1] * 1 / 2 * A[i + 1][j][1] * 2/3)
+                # _A[i][j][1] = 1 - (A[i][j + 1][1] * 3/4 * A[i - 1][j][1] * 1 / 2 * A[i + 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j + 1], A[i - 1][j], A[i + 1][j]))
 
             if i == 1 and j == (DIM - 1):
-                _A[i][j][1] = 1 - (A[i][j - 1][1] * 3/4 * A[i - 1][j][1] * 1/2 * A[i + 1][j][1] * 2/3)
+                # _A[i][j][1] = 1 - (A[i][j - 1][1] * 3/4 * A[i - 1][j][1] * 1/2 * A[i + 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j - 1], A[i - 1][j], A[i + 1][j]))
                 # # # # #
                 # # # # *
                 # # # # #
@@ -178,7 +232,8 @@ def bell():
                 # # # # #
 
             if i == 1 and j == (DIM - 2):
-                _A[i][j][1] = 1 - (A[i][j - 1][1] * 3/4 * A[i][j + 1][1] * 2/3 * A[i - 1][j][1] * 2/3 * A[i + 1][j][1] * 3/4)
+                # _A[i][j][1] = 1 - (A[i][j - 1][1] * 3/4 * A[i][j + 1][1] * 2/3 * A[i - 1][j][1] * 2/3 * A[i + 1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j - 1],A[i][j + 1],A[i - 1][j], A[i + 1][j]))
                 # # # # #
                 # # # * #
                 # # # # #
@@ -188,10 +243,11 @@ def bell():
             # ######## i == 0 ##################################################################################
 
             if i == 0 and j == 0:
-                _A[i][j][1] = 1 - (
-                    A[i][j + 1][1] * 2/3 * A[i + 1][j][1] * 2/3
-                    # + A[i][j + 1][1] * 2/3 * A[i + 1][j][0] + A[i][j + 1][0] * A[i + 1][j][1] * 2/3
-                )
+                # _A[i][j][1] = 1 - (
+                    # A[i][j + 1][1] * 2/3 * A[i + 1][j][1] * 2/3
+                     # + A[i][j + 1][1] * 2/3 * A[i + 1][j][0] + A[i][j + 1][0] * A[i + 1][j][1] * 2/3 + A[i][j + 1][0] * A[i + 1][j][0]
+                # )
+                _A[i][j][0] = comb((A[i][j + 1], A[i + 1][j]))
 #               * # # # #
                 # # # # #
                 # # # # #
@@ -201,14 +257,28 @@ def bell():
             if i == 0 and j == 1:
                 # at least one of the neighboors must give the "1" =>
                 # 1- none of the neighboors give the "1"
-                _A[i][j][1] = 1 - (A[i][j + 1][1] * 2/3 * A[i][j - 1][1] * 1 / 2 * A[i + 1][j][1] * 3/4)
+                # _A[i][j][1] = 1 - (
+                #     A[i][j + 1][1] * 2/3 * A[i][j - 1][1] * 1 / 2 * A[i + 1][j][1] * 3/4
+                #     + A[i][j + 1][1] * 2/3 * A[i][j - 1][1] * 1 / 2 * A[i + 1][j][0]
+                #     + A[i][j + 1][1] * 2 / 3 * A[i][j - 1][0] * A[i + 1][j][1] * 3 / 4
+                #     + A[i][j + 1][0] * A[i][j - 1][1] * 1 / 2 * A[i + 1][j][1] * 3 / 4
+                #     + A[i][j + 1][1] * 2/3 * A[i][j - 1][0] * A[i + 1][j][0]
+                #     + A[i][j + 1][0] * A[i][j - 1][1] * 1 / 2 * A[i + 1][j][0]
+                #     + A[i][j + 1][0] * A[i][j - 1][0] * A[i + 1][j][1] * 3/4
+                #     + A[i][j + 1][0] * A[i][j - 1][0] * A[i + 1][j][0]
+                #
+                #                    )
+                _A[i][j][0] = comb((A[i][j + 1],A[i][j - 1],A[i + 1][j]))
+
+
                 # * # # #
                 # # # # #
                 # # # # #
                 # # # # #
                 # # # # #
             if i == 0 and j == (DIM - 1):
-                _A[i][j][1] = 1 - (A[i][j - 1][1] * 2/3 * A[i + 1][j][1] * 2/3)
+                # _A[i][j][1] = 1 - (A[i][j - 1][1] * 2/3 * A[i + 1][j][1] * 2/3)
+                _A[i][j][0] = comb((A[i][j - 1],A[i + 1][j]))
                 # # # # *
                 # # # # #
                 # # # # #
@@ -216,7 +286,8 @@ def bell():
                 # # # # #
 
             if i == 0 and j == (DIM - 2):
-                _A[i][j][1] = 1 - (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 1/2 * A[i + 1][j][1] * 3/4)
+                # _A[i][j][1] = 1 - (A[i][j - 1][1] * 2/3 * A[i][j + 1][1] * 1/2 * A[i + 1][j][1] * 3/4)
+                _A[i][j][0] = comb((A[i][j - 1], A[i][j + 1],A[i + 1][j]))
                 # # # * #
                 # # # # #
                 # # # # #
@@ -224,28 +295,50 @@ def bell():
                 # # # # #
             # ######## i== DIM-2 ##################################################################################
 
-            _A[i][j][0] = 1.0 - _A[i][j][1]
+            # _A[i][j][0] = 1.0 - _A[i][j][1]
+            _A[i][j][1] = 1.0 - _A[i][j][0]
     A = _A
 
 
 
+
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+
+def bell2():
+    global A
+    global DIM
+    # _A = copy.deepcopy(A)
+    _A = [[[0.0 for x in xrange(2)] for i in xrange(DIM)] for j in xrange(DIM)]
+
+
+
+
+    for i in xrange(DIM):
+        directionsI = 2
+        if i == DIM-1 or i == 0:
+            directionsI -= 1
+        for j in xrange(DIM):
+            directionsJ = 2
+            if j == DIM-1 or j == 0:
+                directionsJ -= 1
+            # P1 = 1.0 / (directionsI + directionsJ)
+            # P1 = ((directionsI + directionsJ)-1.0) / (directionsI + directionsJ)
+            P1 = A[i][j][1] * 1.0 / (directionsI + directionsJ)
+            if i > 0: _A[i-1][j][1] += P1
+            if i < DIM-1: _A[i+1][j][1] += P1
+            if j > 0: _A[i][j-1][1] += P1
+            if j < DIM-1: _A[i][j+1][1] += P1
+            # _A[i][j][1] -=1
+    A = _A
+
+# examines each flea , intead of each square...
+
 for j in xrange(1):
-    bell()
-
-    # exp0 = 0
-    # exp1 = 0
-    #
-    # for i in xrange(DIM):
-    #     for j in xrange(DIM):
-    #         exp0 += A[i][j][0]
-    #         exp1 += A[i][j][1]
-    #
-    # print("Exp '0's: %.6f" % exp0)
-    # print("Exp '>1's: %.6f" % exp1)
-    # print("Total: %.6f" % (exp1 + exp0))
-    #
-    # printA()
-
+    bell2()
 
 printA()
 
@@ -254,8 +347,8 @@ exp1 = 0
 
 for i in xrange(DIM):
     for j in xrange(DIM):
-        exp0 += A[i][j][0]
         exp1 += A[i][j][1]
+        exp0 += 1-A[i][j][1]
 
 print("Exp '0's: %.6f" % exp0)
 print("Exp '>1's: %.6f" % exp1)
